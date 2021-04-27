f0de9278bf4cf3629e09c29b10b3f257c3ba13ab
Justin Elmore <jelmore1674@pm.me>
2021-04-26 21:57:11 -0500

    Added grid, with hover

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 /** @format */
 
-const container = document.querySelector(".container");
-const div = document.createElement("div");
-container.appendChild(div);
+const container = document.getElementById("container");
+
+function makeRows(rows, cols) {
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    for (c = 0; c < rows * cols; c++) {
+        let cell = document.createElement("div");
+        cell.innerText = c + 1;
+        container.appendChild(cell).className = "grid-item";
+    }
+}
+
+function getGrid() {
+    let row = prompt("Select Grid size 4-64");
+
+    if (row < 4) {
+        row = 4;
+    } else if (row >= 4) {
+        row = row;
+    }
+    let col = row;
+    makeRows(row, col);
+}
+
+getGrid();
