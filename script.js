ca278670ebcb77eb361c1992ccd25c2d4ffe38aa
Justin Elmore <jelmore1674@pm.me>
2021-04-27 19:24:18 -0500

    Have it fully functional. Still has styling to do

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 /** @format */
 
 const container = document.getElementById("container");
+const resetBtn = document.getElementById("reset");
 
-function erase() {
-    const gridItems = document.querySelectorAll("#container > div");
-
-    gridItems.forEach((item) => {
-        item.style.backgroundColor = "#fff";
-        item.style.opacity = "1";
-    });
-}
-
-function makeRows(rows, cols) {
-    container.style.setProperty("--grid-rows", rows);
-    container.style.setProperty("--grid-cols", cols);
-    for (c = 0; c < rows * cols; c++) {
+function makeRows(size) {
+    container.innerHTML = "";
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    for (c = 0; c < size * size; c++) {
         let cell = document.createElement("div");
-        cell.innerText = c + 1;
         container.appendChild(cell).className = "grid-item";
         cell.addEventListener("mouseover", paint);
     }
 }
 
 function getGrid() {
-    row = prompt("Select Grid size 4-100");
+    row = prompt("Select Grid Size 4-100");
     if (row < 4) {
         row = 4;
     } else if (row >= 100) {
@@ -31,28 +22,32 @@ function getGrid() {
     } else {
         row = row;
     }
-    col = row;
-    makeRows(row, col);
+    size = row;
+    makeRows(size);
 }
 
-function reset() {
-    const gridItems = document.querySelectorAll("#container > div");
-
-    gridItems.forEach((item) => {
-        item.style.backgroundColor = "#fff";
-        item.style.opacity = "1";
-    });
+function clearGrid() {
+    const gridCount = Math.sqrt(container.childElementCount);
+    makeRows(gridCount);
 }
 
 function paint(e) {
-    const colorChoice = ["#3d5a80", "#98c1d9", "#e0fbfc", "#ee6c4d", "#293241"];
+    const colorChoice = [
+        "green",
+        "yellow",
+        "purple",
+        "red",
+        "blue",
+        "pink",
+        "orange",
+    ];
     const randomColor = Math.floor(Math.random() * colorChoice.length);
-    e.target.style.opacity = 0.6;
+    e.target.style.opacity = 0.8;
     e.target.style.backgroundColor = colorChoice[randomColor];
 }
 
 function game() {
-    getGrid(16, 16);
+    makeRows(24);
 }
-
+resetBtn.addEventListener("click", clearGrid);
 game();
